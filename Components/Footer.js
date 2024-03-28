53a4e1d2ae8d17d6979a613f8dc2e6991e105202
Zeta002 <angeloportelli05@gmail.com>
2024-03-28 20:47:12 +0100

    Avancement Footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,8 +1,27 @@
-import {Text} from "react-native";
+import {TextInput, View} from "react-native";
 
 function Footer() {
     return (
-      <Text>Hello from footer</Text>
+        <View style={{
+            width: '100%',
+            height: 80,
+            backgroundColor: 'darkgray',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            bottom: 0,
+        }}>
+            <TextInput style={{
+                height: 25,
+                width: '80%',
+                borderColor: 'black',
+                backgroundColor: 'white',
+                borderWidth: 1,
+                padding: 5,
+                borderRadius: 5,
+            }}
+            placeholder={"Find a character.."}/>
+        </View>
     );
 }
 
