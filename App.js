2df7d9ccae79f41909dd5e3c6c4e94946814a100
MARIE Clément <clement.marie24@gmail.com>
2024-04-02 08:17:31 +0200

    test

--- App.js
+++ App.js
@@ -1,37 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
     const [character, setCharacter] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('https://genshin.jmp.blue/characters')
-            .then(response => response.json())
-            .then(data => {
+        const fetchData = async () => {
+            try {
+                // Récupérer les données de l'API
+                const response = await fetch('https://genshin.jmp.blue/characters');
+                const data = await response.json();
+
+                // Attendre que les données soient reçues avant de les traiter
+                await new Promise(resolve => setTimeout(resolve, 5000));
+
+                // Une fois les données reçues, traiter les données et mettre à jour l'état
                 const randomIndex = Math.floor(Math.random() * data.length);
                 const randomCharacter = data[randomIndex];
-                console.log(randomCharacter);
                 setCharacter(randomCharacter);
-            })
-            .catch((error) => {
+                setLoading(false);
+            } catch (error) {
                 console.error('Erreur:', error);
-            });
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, []);
 
-    const handlePress = () => {
-        Alert.alert('Button pressed!', 'You pressed the Guess button.');
-    };
-
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            {character ? (
-                <>
-                    <Text>{`Name: ${character.name ? character.name : 'Loading...'}`}</Text>
-                    <Button title="Guess" onPress={handlePress} />
-                </>
-            ) : (
-                <Text>Loading...</Text>
-            )}
-        </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {loading ? (
+            <Text>Chargement...</Text>
+          ) : (
+            <Text>{`Nom: ${character.name}`}</Text>
+          )}
+      </View>
     );
 }
