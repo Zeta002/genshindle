3d7fcd29a409e35d450f0ed8abae4a41f72cc751
Sir Septimus <125373660+AuroreMOMYM22004066@users.noreply.github.com>
2024-04-02 09:08:29 +0200

    modification header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import react, {_Image, Image, Text, View} from "react-native";
+import react, {Image, Text, View} from "react-native";
 
 function Header () {
  const uri ="https://static.wikia.nocookie.net/gensin-impact/images/4/4e/A_Thousand_Questions_With_Paimon_2021-06-25_Thinking.gif/revision/latest?cb=20210628233812"
