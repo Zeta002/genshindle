6a5bb26731c50b0979fa10be355293b9602ec119
Zeta002 <angeloportelli05@gmail.com>
2024-04-02 19:09:21 +0200

    Re-indentation, component Picker toujours infonctionnel

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-        <Footer />
+      <Footer />
     </View>
   );
 }
--- Components/Footer.js
+++ Components/Footer.js
@@ -3,39 +3,39 @@ import {TextInput, View} from "react-native";
 import {Picker} from "@react-native-picker/picker";
 
 function Footer() {
-    const [selectedValue, setSelectedValue] = useState(null);
+  const [selectedValue, setSelectedValue] = useState(null);
 
-    return (
-        <View style={{
-            width: '100%',
-            height: 80,
-            backgroundColor: 'darkgray',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'absolute',
-            bottom: 0,
-        }}>
-            <TextInput style={{
-                height: 25,
-                width: '80%',
-                borderColor: 'black',
-                backgroundColor: 'white',
-                borderWidth: 1,
-                padding: 5,
-                borderRadius: 5,
-            }}
-                       placeholder={"Find a character.."}/>
-            <Picker
-                selectedValue={selectedValue}
-                onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-            >
-                <Picker.Item label="Amber" value="Amber Test" />
-                <Picker.Item label="Albedo" value="Albedo Test" />
-                <Picker.Item label="Zhongli" value="Zhongli Test" />
-                <Picker.Item label="Hu-tao" value="Hu-tao Test" />
-            </Picker>
-        </View>
-    );
+  return (
+    <View style={{
+      width: '100%',
+      height: 80,
+      backgroundColor: 'darkgray',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      bottom: 0,
+    }}>
+      <TextInput style={{
+        height: 25,
+        width: '80%',
+        borderColor: 'black',
+        backgroundColor: 'white',
+        borderWidth: 1,
+        padding: 5,
+        borderRadius: 5,
+      }}
+                 placeholder={"Find a character.."}/>
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Amber" value="Amber Test" />
+        <Picker.Item label="Albedo" value="Albedo Test" />
+        <Picker.Item label="Zhongli" value="Zhongli Test" />
+        <Picker.Item label="Hu-tao" value="Hu-tao Test" />
+      </Picker>
+    </View>
+  );
 }
 
 export default Footer;
