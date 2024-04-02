9d1014ba442b7ff9abd9bb202546023d1614760e
Sir Septimus <125373660+AuroreMOMYM22004066@users.noreply.github.com>
2024-04-02 09:11:25 +0200

    modification header

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,22 @@
 import react, {Image, Text, View} from "react-native";
 
 function Header () {
- const uri ="https://static.wikia.nocookie.net/gensin-impact/images/4/4e/A_Thousand_Questions_With_Paimon_2021-06-25_Thinking.gif/revision/latest?cb=20210628233812"
-    return (
+  const uri ="https://static.wikia.nocookie.net/gensin-impact/images/4/4e/A_Thousand_Questions_With_Paimon_2021-06-25_Thinking.gif/revision/latest?cb=20210628233812"
+  return (
+    <View>
+      <Text>Genshindle</Text>
+      <View>
+        <Image source={
+          {uri}} width={120} height={80}></Image>
         <View>
-            <Text>Genshindle</Text>
-            <View>
-                <Image source={
-                    {uri}} width={120} height={80}></Image>
-                <View>
-                    <Text>try 5/5</Text>
-                    <Text>Winstreak : </Text>
-                    <Text>Best Winstreak : </Text>
-                </View>
-            </View>
+          <Text>try 5/5</Text>
+          <Text>Winstreak : </Text>
+          <Text>Best Winstreak : </Text>
         </View>
+      </View>
+    </View>
 
-    );
+  );
 }
 
 export default Header;
