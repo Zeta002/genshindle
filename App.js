e1e3fe31b959f02c2dd555f07c4aa4c485feb693
Sir Septimus <125373660+AuroreMOMYM22004066@users.noreply.github.com>
2024-04-02 09:10:09 +0200

    modification header

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <View style={styles.container}>
-        <Header/>
+      <Header/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
