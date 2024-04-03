839bbb6acac2d553b86e6cb7a35c16eede8d41c6
Quentin <quentinb2004@gmail.com>
2024-04-04 01:20:23 +0200

    fix

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Header from "./Components/Header";
 
 export default function App() {
   return (
-    <View style={styles.container}>      
-      <Header/>
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#ecf0f1" />
+      <Header />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
       <Footer />
     </View>
   );
